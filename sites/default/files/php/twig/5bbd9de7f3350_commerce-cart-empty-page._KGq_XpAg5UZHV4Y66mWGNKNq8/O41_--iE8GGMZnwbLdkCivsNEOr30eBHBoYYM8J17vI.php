8892aefdc3de0327c6129455b4f7858fb10ecc74
shahiq63 <syed.shahiq@square63.com>
2018-10-10 16:47:40 +0500

    Modules added

---
+++ sites/default/files/php/twig/5bbd9de7f3350_commerce-cart-empty-page._KGq_XpAg5UZHV4Y66mWGNKNq8/O41_--iE8GGMZnwbLdkCivsNEOr30eBHBoYYM8J17vI.php
@@ -0,0 +1,79 @@
+<?php
+
+/* modules/commerce/modules/cart/templates/commerce-cart-empty-page.html.twig */
+class __TwigTemplate_77a027d1313944b0508233b8c6bba897b3d327003488f576714ee508458129f8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array("t" => 11);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array('t'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 10
+        echo "<div class=\"cart-empty-page\">
+  ";
+        // line 11
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Your shopping cart is empty.")));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/commerce/modules/cart/templates/commerce-cart-empty-page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 11,  43 => 10,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/commerce/modules/cart/templates/commerce-cart-empty-page.html.twig", "/var/www/html/online_shop/modules/commerce/modules/cart/templates/commerce-cart-empty-page.html.twig");
+    }
+}
---
+++ sites/default/files/php/twig/5bbd9de7f3350_commerce-promotion-form.h_lxjcsmqVc0fxNEx97pNuxLAdA/jMufHdl4kF_JHXbNMhC_hGsy8JC0L6c1dXjEKvWzmbA.php
@@ -0,0 +1,93 @@
+<?php
+
+/* modules/commerce/modules/promotion/templates/commerce-promotion-form.html.twig */
+class __TwigTemplate_53c6fa6c3128d774ca0bb36adc1dd3729032bef5d7ecd7d78cb49d934701ff12 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array("without" => 14);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array('without'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 12
+        echo "<div class=\"layout-promotion-form clearfix\">
+  <div class=\"layout-region layout-region-promotion-main\">
+    ";
+        // line 14
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_without(($context["form"] ?? null), "advanced", "actions"), "html", null, true));
+        echo "
+  </div>
+  <div class=\"layout-region layout-region-promotion-secondary\">
+    ";
+        // line 17
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["form"] ?? null), "advanced", array()), "html", null, true));
+        echo "
+  </div>
+  <div class=\"layout-region layout-region-promotion-footer\">
+    ";
+        // line 20
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["form"] ?? null), "actions", array()), "html", null, true));
+        echo "
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/commerce/modules/promotion/templates/commerce-promotion-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 20,  53 => 17,  47 => 14,  43 => 12,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/commerce/modules/promotion/templates/commerce-promotion-form.html.twig", "/var/www/html/online_shop/modules/commerce/modules/promotion/templates/commerce-promotion-form.html.twig");
+    }
+}
